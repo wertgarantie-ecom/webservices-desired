e478fcfb4224395cc8e6d325f9a6fd9b767ec51d
henning <henning.nobbe@innoq.com>
2020-04-14 15:51:22 +0200

    Desired Webservices Endpoint

---
+++ api/productOffersExample.js
@@ -0,0 +1,395 @@
+const offers = [
+    {
+        "name": "Komplettschutz",
+        "id": "productOfferUuid",
+        "clientId": "testClientId",
+        "documents": [
+            {
+                "document_title": "GU WG DE KS 0419_RECHTSDOKUMENTE.PDF",
+                "document_type": "LN"
+            }
+        ],
+        "advantages": [],
+        "devices": [
+            {
+                "objectCode": "9025",
+                "objectCodeExternal": "Smartphone",
+                "maxPriceLimitation": "1800",
+                "intervals": [
+                    {
+                        "intervalCode": "1",
+                        "description": "monatlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "3",
+                        "description": "vierteljährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "6",
+                        "description": "halbjährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "12",
+                        "description": "jährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "objectCode": "73",
+                "objectCodeExternal": "Mobilfunk",
+                "maxPriceLimitation": "1800",
+                "intervals": [
+                    {
+                        "intervalCode": "1",
+                        "description": "monatlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "3",
+                        "description": "vierteljährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "6",
+                        "description": "halbjährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "12",
+                        "description": "jährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        "name": "Komplettschutz mit Premium-Option",
+        "id": "productOfferUuid",
+        "clientId": "testClientId",
+        "documents": [
+            {
+                "document_title": "GU WG DE KS 0419_RECHTSDOKUMENTE.PDF",
+                "document_type": "LN"
+            }
+        ],
+        "advantages": [],
+        "devices": [
+            {
+                "objectCode": "9025",
+                "objectCodeExternal": "Smartphone",
+                "maxPriceLimitation": "1800",
+                "intervals": [
+                    {
+                        "intervalCode": "1",
+                        "description": "monatlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "3",
+                        "description": "vierteljährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "6",
+                        "description": "halbjährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "12",
+                        "description": "jährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    }
+                ]
+            },
+            {
+                "objectCode": "73",
+                "objectCodeExternal": "Mobilfunk",
+                "maxPriceLimitation": "1800",
+                "intervals": [
+                    {
+                        "intervalCode": "1",
+                        "description": "monatlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "3",
+                        "description": "vierteljährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "6",
+                        "description": "halbjährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    },
+                    {
+                        "intervalCode": "12",
+                        "description": "jährlich",
+                        "priceRangePremiums": [
+                            {
+                                "minClose": 0,
+                                "maxOpen": 300,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 300,
+                                "maxOpen": 800,
+                                "insurancePremium": "23,4"
+                            },
+                            {
+                                "minClose": 800,
+                                "maxOpen": 1800,
+                                "insurancePremium": "23,4"
+                            }
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+];
+
